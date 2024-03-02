fcd7d4c196e6f5701595811cf1e271977d93be8c
PADILLA-PATRICK-BSIT32E3 <patrickpadilla527@gmail.com>
2024-03-02 15:16:51 +0800

    initial commit

---
+++ RELIM_E3_PadillaPatrick_BSITE3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RELIM_E3_PadillaPatrick_BSITE3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
